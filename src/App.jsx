714324aea006c3abbdaf5cefcad935dacae0fff9
elijuggz <alimansouri.dev@gmail.com>
2024-09-04 08:46:27 -0400

    Environment setup with ReactJS - Vite - TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-950">
+      <header></header>
+      <h1 className=" text-orange-900">Yoo</h1>
+    </div>
+  );
+}
+
+export default App;
